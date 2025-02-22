9a48d22ce4e16404c1971dc56d974c6ae740647c
Cairry <115769353+Cairry@users.noreply.github.com>
2025-02-22 21:29:58 +0800

    🎸 Optimized user alarm subscribe logic

--- alert/consumer/subscribe.go
+++ alert/consumer/subscribe.go
@@ -2,6 +2,7 @@ package consumer
 
 import (
 	"fmt"
+	"github.com/zeromicro/go-zero/core/logc"
 	"strings"
 	"watchAlert/internal/models"
 	"watchAlert/pkg/ctx"
@@ -17,54 +18,83 @@ type toUser struct {
 }
 
 // 向已订阅的用户中发送告警消息
-func processSubscribe(ctx *ctx.Context, alert *models.AlertCurEvent, notice models.AlertNotice) error {
+func processSubscribe(ctx *ctx.Context, alert *models.AlertCurEvent) error {
+	var toUsers []toUser
+
+	// 获取所有用户订阅列表
+	subscribes, err := getSubscribes(alert)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("subscribes->", subscribes)
+
+	for _, subscribe := range subscribes {
+		// Severity检查
+		severitySet := make(map[string]struct{})
+		for _, sev := range subscribe.SRuleSeverity {
+			severitySet[sev] = struct{}{}
+		}
+		if _, exists := severitySet[alert.Severity]; !exists {
+			continue
+		}
+
+		// 过滤器检查
+		if len(subscribe.SFilter) > 0 {
+			allMatched := true
+			for _, f := range subscribe.SFilter {
+				if !strings.Contains(tools.JsonMarshal(alert.Metric), f) && !strings.Contains(alert.Annotations, f) {
+					allMatched = false
+					break
+				}
+			}
+			if !allMatched {
+				continue
+			}
+		}
+
+		toUsers = append(toUsers, toUser{
+			Email:            subscribe.SUserEmail,
+			NoticeSubject:    subscribe.SNoticeSubject,
+			NoticeTemplateId: subscribe.SNoticeTemplateId,
+		})
+		fmt.Println("toUsers->", toUsers)
+	}
+
+	return sendToSubscribeUser(ctx, *alert, toUsers)
+}
+
+func getSubscribes(alert *models.AlertCurEvent) ([]models.AlertSubscribe, error) {
+	// Firing_a-csu3be5vi7ns2tmpdnhg
+	ruleId := strings.Split(alert.RuleId, "_")
 	list, err := ctx.DB.Subscribe().List(models.AlertSubscribeQuery{
 		STenantId: alert.TenantId,
-		Query:     alert.RuleId,
+		SRuleId:   ruleId[1],
 	})
 	if err != nil {
-		return fmt.Errorf("获取订阅用户失败, err: %s", err.Error())
+		return nil, fmt.Errorf("获取订阅用户失败, err: %s", err.Error())
 	}
 
-	notice.NoticeType = "Email"
-	var toUsers []toUser
-	for _, s := range list {
-		var foundSeverity, foundFilter bool
-		for _, severity := range s.SRuleSeverity {
-			if severity == alert.Severity {
-				foundSeverity = true
-				break
-			}
-		}
+	return list, nil
+}
 
-		if foundSeverity {
-			if len(s.SFilter) > 0 {
-				for _, f := range s.SFilter {
-					if strings.Contains(tools.JsonMarshal(alert.Metric), f) || strings.Contains(alert.Annotations, f) {
-						foundFilter = true
-						break
-					}
-				}
-			} else {
-				foundFilter = true
-			}
-
-			if foundFilter {
-				toUsers = append(toUsers, toUser{
-					Email:            s.SUserEmail,
-					NoticeSubject:    s.SNoticeSubject,
-					NoticeTemplateId: s.SNoticeTemplateId,
-				})
-			}
-		}
+func sendToSubscribeUser(ctx *ctx.Context, alert models.AlertCurEvent, toUsers []toUser) error {
+	if len(toUsers) <= 0 {
+		return nil
 	}
 
-	if len(toUsers) > 0 {
-		for _, u := range toUsers {
-			notice.NoticeTmplId = u.NoticeTemplateId
-			emailTemp := templates.NewTemplate(ctx, *alert, notice)
-
-			err = sender.NewEmailSender().Send(sender.SendParams{
+	var sem = make(chan struct{}, 10)
+	for _, user := range toUsers {
+		u := user
+		// 插入信号量，超过 10 则阻塞协程启动
+		sem <- struct{}{}
+		go func(u toUser, sem chan struct{}) {
+			defer func() {
+				// 释放信号量
+				<-sem
+			}()
+			emailTemp := templates.NewTemplate(ctx, alert, models.AlertNotice{NoticeType: "Email", NoticeTmplId: u.NoticeTemplateId})
+			err := sender.NewEmailSender().Send(sender.SendParams{
 				IsRecovered: alert.IsRecovered,
 				Email: models.Email{
 					Subject: u.NoticeSubject,
@@ -74,9 +104,9 @@ func processSubscribe(ctx *ctx.Context, alert *models.AlertCurEvent, notice mode
 				Content: emailTemp.CardContentMsg,
 			})
 			if err != nil {
-				return fmt.Errorf("邮件发送失败, err: %s", err.Error())
+				logc.Errorf(ctx.Ctx, fmt.Sprintf("Email: %s, 邮件发送失败, err: %s", u.Email, err.Error()))
 			}
-		}
+		}(u, sem)
 	}
 
 	return nil
