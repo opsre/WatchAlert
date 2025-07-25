348e455f9fd80c31688b05f018d6bd65aeabc55a
Cairry <115769353+Cairry@users.noreply.github.com>
2025-07-25 12:09:23 +0800

    ♻️ Remove people struct

--- internal/models/duty_schedule.go
+++ internal/models/duty_schedule.go
@@ -1,31 +1,5 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
-type People struct {
-	gorm.Model
-	UserID         string `gorm:"column:userId" json:"userId"`
-	UserName       string `gorm:"column:userName" json:"userName"`
-	Phone          string `gorm:"column:phone" json:"phone"`
-	Email          string `gorm:"column:email" json:"email"`
-	Notice         string `gorm:"column:sender" json:"sender"`
-	FeiShuUserID   string `gorm:"column:feiShuUserID" json:"feiShuUserID"`
-	DingDingUserID string `gorm:"column:dingDingUserID" json:"dingDingUserID"`
-}
-
-type PeopleGroup struct {
-	gorm.Model
-	GroupID   uint   `gorm:"column:groupID" json:"groupID"`
-	GroupName string `gorm:"column:groupName" json:"groupName"`
-}
-
-type JoinsPeopleGroup struct {
-	UserName  string
-	GroupName string
-}
-
 type DutyManagement struct {
 	TenantId    string     `json:"tenantId"`
 	ID          string     `json:"id"`
