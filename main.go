87f44dad077395380b4990e9f78674024dfc06e6
Cairry <115769353+Cairry@users.noreply.github.com>
2025-09-28 16:27:08 +0800

    ✨ Increase pprof tool

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	_ "net/http/pprof"
 	"watchAlert/initialization"
 	"watchAlert/internal/global"
 )
@@ -9,6 +11,11 @@ var Version string
 
 func main() {
 	global.Version = Version
+
+	go func() {
+		panic(http.ListenAndServe("localhost:9999", nil))
+	}()
+
 	initialization.InitBasic()
 	initialization.InitRoute()
 }
